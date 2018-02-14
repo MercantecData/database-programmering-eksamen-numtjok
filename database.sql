0267e7adcbf9ceec903474ecbc0a634568a623e5
DESKTOP-OMKBHQ9\Jack <numtjok1234@gmail.com>
2018-02-14 12:26:41 +0100

    database

--- database.sql
+++ database.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Vært: 127.0.0.1
--- Genereringstid: 13. 02 2018 kl. 15:40:36
--- Serverversion: 10.1.26-MariaDB
--- PHP-version: 7.1.9
+-- Genereringstid: 14. 02 2018 kl. 12:25:59
+-- Serverversion: 10.1.28-MariaDB
+-- PHP-version: 7.1.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -62,7 +62,6 @@ INSERT INTO `images` (`id`, `imageURL`, `owner`) VALUES
 (2, 'https://imgix.ranker.com/user_node_img/50047/1000931279/original/millennium-falcon-photo-u1?w=650&q=50&fm=jpg&fit=crop&crop=faces', 1),
 (3, 'https://imgix.ranker.com/user_node_img/50047/1000931306/original/vader-and-39-s-tie-fighter-photo-u1?w=650&q=50&fm=jpg&fit=crop&crop=faces', 1),
 (4, 'https://imgix.ranker.com/user_node_img/50047/1000931297/original/star-destroyer-photo-u1?w=650&q=50&fm=jpg&fit=crop&crop=faces', 1),
-(5, 'https://imgix.ranker.com/user_node_img/50047/1000931296/original/slave-i-photo-u1?w=650&q=60&fm=jpg&fit=crop&crop=faces', 1),
 (6, 'http://static.tvtropes.org/pmwiki/pub/images/friendship_is_magic_newpageimage_1684.png', 2),
 (7, 'https://vignette.wikia.nocookie.net/mugen/images/1/13/Fluttershy_artwork.png/revision/latest?cb=20130311003323', 2);
 
@@ -73,7 +72,7 @@ INSERT INTO `images` (`id`, `imageURL`, `owner`) VALUES
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
   `username` varchar(32) NOT NULL,
   `password` varchar(32) NOT NULL,
   `name` varchar(32) NOT NULL
@@ -83,7 +82,7 @@ CREATE TABLE `users` (
 -- Data dump for tabellen `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `name`) VALUES
+INSERT INTO `users` (`UserID`, `username`, `password`, `name`) VALUES
 (1, 'kyloren', 'darth4ever', 'Ben Solo'),
 (2, 'fShy34', 'friendshipism4gic', 'Fluttershy');
 
@@ -108,7 +107,7 @@ ALTER TABLE `images`
 -- Indeks for tabel `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`UserID`);
 
 --
 -- Brug ikke AUTO_INCREMENT for slettede tabeller
@@ -124,13 +123,13 @@ ALTER TABLE `adminusers`
 -- Tilføj AUTO_INCREMENT i tabel `images`
 --
 ALTER TABLE `images`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Tilføj AUTO_INCREMENT i tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Begrænsninger for dumpede tabeller
@@ -140,7 +139,7 @@ ALTER TABLE `users`
 -- Begrænsninger for tabel `images`
 --
 ALTER TABLE `images`
-  ADD CONSTRAINT `foreign key` FOREIGN KEY (`owner`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `foreign key` FOREIGN KEY (`owner`) REFERENCES `users` (`UserID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
