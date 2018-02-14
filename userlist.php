7e70e548002e3c91cf218620c4621fa0e1c56123
DESKTOP-OMKBHQ9\Jack <numtjok1234@gmail.com>
2018-02-14 10:42:16 +0100

    Delete Option Added, No longer links but a delete input. Simple.

--- userlist.php
+++ userlist.php
@@ -1,6 +1,6 @@
 <?php
 	$conn = mysqli_connect("localhost", "root", "", "databaseexam");
-	$sql = "SELECT UserID, name FROM users WHERE 1";
+	$sql = "SELECT UserID, username FROM users WHERE 1";
 	$result = $conn->query($sql);
 ?>
 
@@ -14,9 +14,22 @@
 	<h1>Users:</h1>
 	<?php 
 	while($row = $result->fetch_assoc()){
-		echo $row["name"];
-		echo "   <a href='deleteuser.php'>delete</a>";
+		echo $row["username"];
+		echo "    </br>";
 	}
 	?>
+	<form method="POST">
+		<input type="text" name="DeleteUser">
+		<input type="submit" name="delete" value="delete">
+
+	</form>
+
+	<?php 
+	if (isset($_POST['delete'])) {
+		$DeleteUserwithname=$_POST['DeleteUser'];
+		$sql='DELETE FROM users WHERE username="'.$DeleteUserwithname.'"';
+		mysqli_query($conn,$sql);
+	}
+	 ?>
 </body>
 </html>
